b8db68675dc990e0979339f7efb424b7d02e67e6
Lireer <cs.cherer@web.de>
2016-11-19 00:24:47 +0100

    Json Einbindung und erste Tests

--- ricochet_board/src/lib.rs
+++ ricochet_board/src/lib.rs
@@ -1,30 +1,32 @@
-use std::collections::BTreeMap;
+extern crate rustc_serialize;
 
+#[derive(RustcDecodable, RustcEncodable, Copy, Clone)]
 pub struct Field {
-    bottom: bool,
-    right: bool,
-    target: Option<Target>,
+    pub bottom: bool,
+    pub right: bool,
+    pub target: Option<Target>,
 }
 
+pub const BOARDSIZE: usize = 16;
 
-const BOARDSIZE: usize = 16;
-
+#[derive(RustcDecodable, RustcEncodable)]
 pub struct Board {
-    fields: [[Field; BOARDSIZE]; BOARDSIZE],
+    pub fields: [[Field; BOARDSIZE]; BOARDSIZE],
 }
 
+#[derive(RustcDecodable, RustcEncodable)]
 pub struct RobotPositions {
-    rob_position: BTreeMap<(usize, usize), Robot>,
+    pub rob_position: [(usize, usize); 4],
 }
 
 #[derive(PartialEq,Copy, Clone)]
 pub enum Robot {
-    Red,
-    Green,
-    Blue,
-    Yellow,
+    Red = 0,
+    Green = 1,
+    Blue = 2,
+    Yellow = 3,
 }
-
+#[derive(RustcDecodable, RustcEncodable, Clone, Copy)]
 pub enum Target {
     Red(Symbol),
     Green(Symbol),
@@ -33,6 +35,7 @@ pub enum Target {
     Spiral,
 }
 
+#[derive(RustcDecodable, RustcEncodable, Clone, Copy)]
 pub enum Symbol {
     Circle,
     Triangle,
@@ -59,41 +62,48 @@ impl Board {
 
     pub fn wall_top(&self, x: usize, y: usize) -> bool {
         if y == 0 {
-            self.wall_right(x, BOARDSIZE - 1)
+            self.wall_bottom(x, BOARDSIZE - 1)
         } else {
-            self.wall_right(x, y - 1)
+            self.wall_bottom(x, y - 1)
         }
     }
 }
 
 impl RobotPositions {
+    fn contains_robot(&self, x: usize, y: usize) -> bool {
+        for &(k, m) in &self.rob_position {
+            if x == k && y == m {
+                return true;
+            }
+        }
+        false
+    }
+
     fn can_move_right(&self, board: &Board, x: usize, y: usize) -> bool {
-        !board.wall_right(x, y) && self.rob_position.contains_key(&((x + 1) % BOARDSIZE, y))
+        !board.wall_right(x, y) && !self.contains_robot((x + 1) % BOARDSIZE, y)
     }
 
     fn can_move_bottom(&self, board: &Board, x: usize, y: usize) -> bool {
-        !board.wall_bottom(x, y) && self.rob_position.contains_key(&(x, (y + 1) % BOARDSIZE))
+        !board.wall_bottom(x, y) && !self.contains_robot(x, (y + 1) % BOARDSIZE)
     }
 
     fn can_move_left(&self, board: &Board, x: usize, y: usize) -> bool {
-        !board.wall_left(x, y) &&
-        self.rob_position.contains_key(&((x + BOARDSIZE - 1) % BOARDSIZE, y))
+        !board.wall_left(x, y) && !self.contains_robot((x + BOARDSIZE - 1) % BOARDSIZE, y)
     }
 
     fn can_move_top(&self, board: &Board, x: usize, y: usize) -> bool {
-        !board.wall_top(x, y) &&
-        self.rob_position.contains_key(&(x, (y + BOARDSIZE - 1) % BOARDSIZE))
+        !board.wall_top(x, y) && !self.contains_robot(x, (y + BOARDSIZE - 1) % BOARDSIZE)
     }
 }
 
 impl RobotPositions {
     pub fn move_right(&mut self, robot: Robot, board: &Board) {
-        let &(x, y) = self.rob_position.iter().find(|&(_, v)| *v == robot).unwrap().0;
+        let (x, y) = self.rob_position[robot as usize];
         for x_tmp in x.. {
+            let x_tmp = x_tmp % BOARDSIZE;
             if !self.can_move_right(board, x_tmp, y) {
                 if x != x_tmp {
-                    self.rob_position.insert((x_tmp, y), robot);
-                    self.rob_position.remove(&(x, y));
+                    self.rob_position[robot as usize] = (x_tmp, y);
                 }
                 // TODO Zielabfrage
                 return;
@@ -102,12 +112,12 @@ impl RobotPositions {
     }
 
     pub fn move_bottom(&mut self, robot: Robot, board: &Board) {
-        let &(x, y) = self.rob_position.iter().find(|&(_, v)| *v == robot).unwrap().0;
+        let (x, y) = self.rob_position[robot as usize];
         for y_tmp in y.. {
+            let y_tmp = y_tmp % BOARDSIZE;
             if !self.can_move_bottom(board, x, y_tmp) {
                 if y != y_tmp {
-                    self.rob_position.insert((x, y_tmp), robot);
-                    self.rob_position.remove(&(x, y));
+                    self.rob_position[robot as usize] = (x, y_tmp);
                 }
                 // TODO Zielabfrage
                 return;
@@ -116,12 +126,12 @@ impl RobotPositions {
     }
 
     pub fn move_left(&mut self, robot: Robot, board: &Board) {
-        let &(x, y) = self.rob_position.iter().find(|&(_, v)| *v == robot).unwrap().0;
+        let (x, y) = self.rob_position[robot as usize];
         for i in 0.. {
-            if !self.can_move_left(board, x - i, y) {
+            let x = (x + BOARDSIZE - i) % BOARDSIZE;
+            if !self.can_move_left(board, x, y) {
                 if i != 0 {
-                    self.rob_position.insert((x - i, y), robot);
-                    self.rob_position.remove(&(x, y));
+                    self.rob_position[robot as usize] = (x, y);
                 }
                 // TODO Zielabfrage
                 return;
@@ -130,12 +140,12 @@ impl RobotPositions {
     }
 
     pub fn move_top(&mut self, robot: Robot, board: &Board) {
-        let &(x, y) = self.rob_position.iter().find(|&(_, v)| *v == robot).unwrap().0;
+        let (x, y) = self.rob_position[robot as usize];
         for i in 0.. {
-            if !self.can_move_top(board, x, y - 1) {
+            let y = (y + BOARDSIZE - i) % BOARDSIZE;
+            if !self.can_move_top(board, x, y) {
                 if i != 0 {
-                    self.rob_position.insert((x, y - i), robot);
-                    self.rob_position.remove(&(x, y));
+                    self.rob_position[robot as usize] = (x, y);
                 }
                 // TODO Zielabfrage
                 return;
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,50 @@
+extern crate ricochet_board;
+extern crate rustc_serialize;
+
+use rustc_serialize::json::*;
+use std::fs::File;
+use std::io::prelude::*;
+use ricochet_board::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut positions = RobotPositions { rob_position: [(1, 4), (14, 14), (4, 5), (6, 7)] };
+    println!("Text 1");
+
+    let mut board = Board {
+        fields: [[Field {
+            bottom: false,
+            right: false,
+            target: None,
+        }; BOARDSIZE]; BOARDSIZE],
+    };
+    println!("Text 2");
+
+    for i in 0..BOARDSIZE {
+        board.fields[i][BOARDSIZE - 1].right = true;
+    }
+
+    for i in 0..BOARDSIZE {
+        board.fields[BOARDSIZE - 1][i].bottom = true;
+    }
+
+    // Test warum rot bei 1,4 rauskommt
+    // positions.move_bottom(Robot::Red, &board);
+    // println!("{0}", positions.rob_position[0]);
+    // positions.move_right(Robot::Red, &board);
+    // println!("{0}", positions.rob_position[0]);
+    // positions.move_top(Robot::Red, &board);
+    // println!("{0}", positions.rob_position[0]);
+    // positions.move_left(Robot::Red, &board);
+    // println!("{0}", positions.rob_position[0]);
+    //
+
+    let mut save = File::create("Test.json").expect("Schreiben der json-Datei");
+
+    println!("Text 3");
+    write!(save,
+           "{0} \n {1}",
+           as_pretty_json(&board),
+           as_pretty_json(&positions))
+        .expect("Die json-Datei beschreiben");
+    println!("Text 4");
 }
