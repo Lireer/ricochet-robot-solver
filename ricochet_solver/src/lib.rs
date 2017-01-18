7494f4b94c993267edc9b4de7849e50bac6d029a
Oliver Schneider <git-no-reply-9879165716479413131@oli-obk.de>
2017-01-18 20:16:57 +0100

    prototype for reverse direction finder

--- ricochet_solver/src/lib.rs
+++ ricochet_solver/src/lib.rs
@@ -33,7 +33,7 @@ impl Entry {
     }
 }
 
-pub fn solve(board: &Board, positions: RobotPositions, target: Target) -> u8 {
+pub fn solve(board: &Board, positions: RobotPositions, target: Target) -> Vec<(Robot, Direction)> {
     let mut database = Database(box [Entry(255); 1 << 32]);
     let (x, y) = board.targets
         .iter()
@@ -47,14 +47,14 @@ pub fn solve(board: &Board, positions: RobotPositions, target: Target) -> u8 {
     for steps in 1.. {
         for j in 0..database.0.len() {
             if database.0[j].steps() == Some(steps) {
-                if eval(board,
+                if let Some(target) = eval(board,
                         RobotPositions(j as u32),
                         &mut database,
                         x,
                         y,
                         steps,
                         target) {
-                    return steps + 1;
+                    return find_direction(steps, &mut database, board, target);
                 }
             }
         }
@@ -63,11 +63,39 @@ pub fn solve(board: &Board, positions: RobotPositions, target: Target) -> u8 {
     unreachable!()
 }
 
+#[derive(Debug)]
+pub enum Direction {
+    Right,
+    Left,
+    Up,
+    Down,
+}
+
 const DIRECTIONS: [fn(&mut RobotPositions, robot: Robot, board: &Board); 4] =
     [RobotPositions::move_right,
      RobotPositions::move_left,
      RobotPositions::move_up,
      RobotPositions::move_down];
+
+/*
+     // mark all bits that differ
+     let diff = pos1 ^ pos2;
+     // find the position of the most right bit that differed
+     let last = diff.trailing_zeros() + 1;
+     // find the position of the most left bit that differed
+     let first = 32 - (diff.leading_zeros() + 1);
+     // the last two bits only tell which bit of the coordinate changed, drop them
+     let last_sector = last >> 2;
+     let first_sector = first >> 2;
+     // if the sector is the same, this is potentially a source location
+     if last_sector == first_sector {
+         // yay
+     }
+*/
+
+fn find_direction(steps: usize, database: &mut Database, board: &Board, target: RobotPositions) -> Vec<(Robot, Direction)> {
+
+}
 
 fn eval(board: &Board,
         start: RobotPositions,
@@ -76,7 +104,7 @@ fn eval(board: &Board,
         target_y: usize,
         steps: u8,
         target: Target)
-        -> bool {
+        -> Option<RobotPositions> {
     let mut new = [[start; 4]; 4];
     for (i, &robot) in [Robot::Red, Robot::Green, Robot::Blue, Robot::Yellow].iter().enumerate() {
         for (j, dir) in DIRECTIONS.iter().enumerate() {
@@ -85,31 +113,31 @@ fn eval(board: &Board,
             match target {
                 Target::Spiral => {
                     if new[i][j].contains_robot(target_x, target_y) {
-                        return true;
+                        return Some(new[i][j]);
                     }
                 }
                 Target::Red(_) => {
                     if robot == Robot::Red && new[i][j].contains_red(target_x, target_y) {
-                        return true;
+                        return Some(new[i][j]);
                     }
                 }
                 Target::Green(_) => {
                     if robot == Robot::Green && new[i][j].contains_green(target_x, target_y) {
-                        return true;
+                        return Some(new[i][j]);
                     }
                 }
                 Target::Blue(_) => {
                     if robot == Robot::Blue && new[i][j].contains_blue(target_x, target_y) {
-                        return true;
+                        return Some(new[i][j]);
                     }
                 }
                 Target::Yellow(_) => {
                     if robot == Robot::Yellow && new[i][j].contains_yellow(target_x, target_y) {
-                        return true;
+                        return Some(new[i][j]);
                     }
                 }
             }
         }
     }
-    false
+    None
 }
